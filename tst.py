58d1a73df37556954207e063142c15a6eacaccf5
YasmineFD <163323623+YasmineFD@users.noreply.github.com>
2024-04-29 23:04:49 +0100

    Update tst.py

--- tst.py
+++ tst.py
@@ -1,13 +1,13 @@
 import streamlit as st
-from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 # Fonction pour analyser le sentiment
 def analyze_sentiment(message):
-    blob = TextBlob(message)
-    sentiment_score = blob.sentiment.polarity
-    if sentiment_score > 0:
+    analyzer = SentimentIntensityAnalyzer()
+    sentiment_score = analyzer.polarity_scores(message)['compound']
+    if sentiment_score > 0.05:
         return "Positif"
-    elif sentiment_score < 0:
+    elif sentiment_score < -0.05:
         return "Négatif"
     else:
         return "Neutre"
@@ -16,7 +16,7 @@ def analyze_sentiment(message):
 st.title("Analyse de sentiments sur les réseaux sociaux")
 
 # Zone de texte pour saisir le message
-message_input = st.text_input("Saisissez votre message :")
+message_input = st.text_area("Saisissez votre message :", height=100)
 
 # Bouton pour analyser le sentiment
 if st.button("Analyser le sentiment"):
@@ -25,4 +25,3 @@ if st.button("Analyser le sentiment"):
         st.write("Sentiment du message :", sentiment)
     else:
         st.write("Veuillez saisir un message pour l'analyser.")
-
